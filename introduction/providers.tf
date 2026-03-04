41ee75d0abf4063a4d5233d7a8b0f8ac435df1ef
mussawirimam <mussawirimam@gmail.com>
2026-03-04 02:42:56 +0000

    history

---
+++ introduction/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.8.1"
+    }
+  }
+}
+
